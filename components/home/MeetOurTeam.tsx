0f17d9e1b0f539051387c5a4acb154777a99a57b
Thai Binh TES <thaibinhtes@gmail.com>
2024-01-09 18:52:17 +0700

    update meet our team

--- components/home/MeetOurTeam.tsx
+++ components/home/MeetOurTeam.tsx
@@ -12,7 +12,7 @@ const MeetOurTeam = () => {
         </div>
 
         <div className="meet-our-team__list grid grid-cols-12 gap-[20px] mt-[112px]">
-          <div className="col-span-4">
+          <div className="col-span-6 md:col-span-4">
             <div className="meet-our-team__item --left">
               <img src="/mr-david.png" alt="Mr.David" />
               <div className="meet-our-team__intro-item">
@@ -25,14 +25,14 @@ const MeetOurTeam = () => {
               </div>
             </div>
           </div>
-          <div className="col-span-8">
+          <div className="col-span-6 md:col-span-8">
             <div className="grid grid-cols-12 gap-[20px] mb-[20px]">
-              <div className="col-span-4">
+              <div className="col-span-12 md:col-span-4">
                 <div className="meet-our-team__item --logo">
                   <img className="meet-our-team__item-logo m-auto" width="180px" height="180px" src="/logo-black.png" alt="Studily" />
                 </div>
               </div>
-              <div className="col-span-4">
+              <div className="col-span-6 md:col-span-4">
                 <div className="meet-our-team__item">
                   <img src="/ms-david.png" alt="Ms.David" />
                   <div className="meet-our-team__intro-item">
@@ -46,7 +46,7 @@ const MeetOurTeam = () => {
                 </div>
               </div>
 
-              <div className="col-span-4">
+              <div className="col-span-6 md:col-span-4">
                 <div className="meet-our-team__item">
                   <img src="/ms-david.png" alt="Ms.David" />
 
@@ -61,7 +61,7 @@ const MeetOurTeam = () => {
                 </div>
               </div>
             </div>
-            <div className="grid grid-cols-12 gap-[20px]">
+            <div className="hidden md:flex grid grid-cols-12 gap-[20px]">
               <div className="col-span-7">
                 <div className="meet-our-team__item">
                   <img src="/ceo.png" alt="CEO" />
@@ -92,6 +92,37 @@ const MeetOurTeam = () => {
               </div>
             </div>
           </div>
+
+          <div className="md:hidden flex col-span-12 grid grid-cols-12 gap-[20px]">
+              <div className="col-span-7">
+                <div className="meet-our-team__item">
+                  <img src="/ceo.png" alt="CEO" />
+
+                  <div className="meet-our-team__intro-item">
+                    <h3 className="meet-out-team__name-team color-white">
+                      Mr.David
+                    </h3>
+                    <span className="meet-out-team__position">
+                      Founder, CEO
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-span-5">
+                <div className="meet-our-team__item">
+                  <img src="/founder.png" alt="founder" />
+                  <div className="meet-our-team__intro-item">
+                    <h3 className="meet-out-team__name-team color-white">
+                      Mr.David
+                    </h3>
+                    <span className="meet-out-team__position">
+                      Founder, CEO
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
         </div>
       </div>
     </section>
